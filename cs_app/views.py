8e838ffe2f2f7bdd937d7bdb31aab046dadf4fbf
Kent Howell <howellk3@miamioh.edu>
2024-04-10 14:24:41 -0400

    Added new login template and view base

--- cs_app/views.py
+++ cs_app/views.py
@@ -4,3 +4,7 @@ from django.template import loader
 def main(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
+
+def login(request):
+    template = loader.get_template("login.html")
+    return HttpResponse(template.render())
