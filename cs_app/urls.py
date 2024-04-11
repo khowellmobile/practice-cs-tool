41ea2c7d3a457c97660d1617b96ea5b6ad8a9803
Kent Howell <howellk3@miamioh.edu>
2024-04-11 15:48:35 -0400

    added account creation

--- cs_app/urls.py
+++ cs_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('login/', views.login_view, name='login'),
     path('success/', views.success, name='success'),
+    path('create_account/', views.create_account, name='create_account'),
 ]
