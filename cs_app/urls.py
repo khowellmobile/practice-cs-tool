0742c333c58a05acf316cd791c2e7fff9f20241b
Kent Howell <howellk3@miamioh.edu>
2024-04-11 10:39:20 -0400

    Added basic login functionality

--- cs_app/urls.py
+++ cs_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
-    path('login/', views.login, name='login')
+    path('login/', views.login_view, name='login'),
+    path('success/', views.success, name='success'),
 ]
--- cs_app/views.py
+++ cs_app/views.py
@@ -1,10 +1,36 @@
 from django.http import HttpResponse
 from django.template import loader
 
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+
 def main(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
 
-def login(request):
-    template = loader.get_template("login.html")
+def success(request):
+    template = loader.get_template('success.html')
     return HttpResponse(template.render())
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                # Redirect to a success page after login
+                return redirect('/success/')
+            else:
+                # Invalid login credentials
+                error_message = "Invalid username or password."
+        else:
+            # Form is not valid
+            error_message = "Invalid form data. Please check the input fields."
+    else:
+        form = AuthenticationForm()
+        error_message = None
+    return render(request, 'login.html', {'form': form, 'error_message': error_message})
