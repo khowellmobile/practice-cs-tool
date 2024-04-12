95443463a50751b89cbd71f483335bc55da21d4f
Kent Howell <howellk3@miamioh.edu>
2024-04-12 15:02:53 -0400

    removed unused imports

--- cs_app/views.py
+++ cs_app/views.py
@@ -5,9 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
-from django.contrib import messages
 
 def main_view(request):
     template = loader.get_template('index.html')
