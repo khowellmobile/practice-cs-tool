ba7e96950bf128675523f72dd1d5c9ec95814106
Kent Howell <howellk3@miamioh.edu>
2024-04-15 09:55:08 -0400

    Added the urls

--- cs_app/urls.py
+++ cs_app/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('create_account/', views.create_account_view, name='create_account'),
     path('home/', views.home_view, name='home'),
+    path('generate_report/', views.generate_report_view, name='generate_report'),
+    path('directions/', views.directions_view, name='directions'),
+    path('change_database/', views.change_database_view, name='change_database'),
+    path('change_account/', views.change_account_view, name='change_account'),
 ]
