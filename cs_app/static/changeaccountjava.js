21191bad4871eed0310ba0d0fb7b0f3fa31ad67e
khowell <khowellmobile@gmail.com>
2024-04-17 08:53:38 -0400

    Formatted code with prettier and black

--- cs_app/static/changeaccountjava.js
+++ cs_app/static/changeaccountjava.js
@@ -1,55 +1,55 @@
 function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = jQuery.trim(cookies[i]);
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
     }
-    return cookieValue;
+  }
+  return cookieValue;
 }
 
 function toggleForm(fieldName) {
-    $("#update_" + fieldName).toggle();
+  $("#update_" + fieldName).toggle();
 }
 
 function submitForm(fieldName) {
-    $("#update_" + fieldName).hide();
+  $("#update_" + fieldName).hide();
 
-    if (fieldName == 'name') {
-        var url = "/change_account/update_name/"
-        var formdata = {
-            'first_name' : $("#first_name").val(),
-            'last_name' : $("#last_name").val(),
-        }
-    } else if (fieldName == 'email') {
-        var url = '/change_account/update_email/'
-        var formdata = {
-            'email' : $("#email").val(),
-        }
-    } else {
-        var url = '/change_account/change_password/'
-        var formdata = {
-            'password' : $("#password").val(),
-        }
-    }
+  if (fieldName == "name") {
+    var url = "/change_account/update_name/";
+    var formdata = {
+      first_name: $("#first_name").val(),
+      last_name: $("#last_name").val(),
+    };
+  } else if (fieldName == "email") {
+    var url = "/change_account/update_email/";
+    var formdata = {
+      email: $("#email").val(),
+    };
+  } else {
+    var url = "/change_account/change_password/";
+    var formdata = {
+      password: $("#password").val(),
+    };
+  }
 
-    $.ajax({
-        type: "POST",
-        headers: {'X-CSRFToken': csrf_token},
-        url: url,
-        data: formdata,
-        success: function(response) {
-            console.log(response);
-            $('#first_name_last_name').text("Name: " + formdata['first_name'] + " " + formdata['last_name'])
-        },
-        error: function(xhr, errmsg, err) {
-            
-        }
-    });
+  $.ajax({
+    type: "POST",
+    headers: { "X-CSRFToken": csrf_token },
+    url: url,
+    data: formdata,
+    success: function (response) {
+      console.log(response);
+      $("#first_name_last_name").text(
+        "Name: " + formdata["first_name"] + " " + formdata["last_name"]
+      );
+    },
+    error: function (xhr, errmsg, err) {},
+  });
 }
