32c641e0f837ccec0c73dd295ec6214e00789f8c
khowellmobile <khowellmobile@gmailc.com>
2024-04-22 09:41:17 -0400

    Added common_script to hold common functions

--- csTool/settings.py
+++ csTool/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cs_app',
-    'bootstrap5',
 ]
 
 MIDDLEWARE = [
---
+++ cs_app/static/js/common_script.js
@@ -0,0 +1,22 @@
+
+/*
+ * Common Script for Django Application
+ *
+ * This script contains common functions and utilities that are used across multiple pages
+ * in the Django application. It helps in maintaining consistency and reusability of code.
+ *
+ * List of functions:
+ * - redirectTo
+ * - [Function 2]
+ * - [Function 3]
+ *   ...
+ */
+
+function redirectTo(url) {
+    window.location.href = url;
+}
+
+
+function getCurrentUser() {
+
+}
