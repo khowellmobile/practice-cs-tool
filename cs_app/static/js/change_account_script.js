8277f36c3c60967d6a91afc481e758b599920353
khowellmobile <khowellmobile@gmailc.com>
2024-04-24 12:17:52 -0400

    Reformatted code. Changed spacing from 2 to 4 spaces.

--- cs_app/static/js/change_account_script.js
+++ cs_app/static/js/change_account_script.js
@@ -1,49 +1,49 @@
 function toggleForm(fieldName) {
-  $("#update_" + fieldName).toggle();
+    $("#update_" + fieldName).toggle();
 }
 
 function submitForm(fieldName) {
-  $("#update_" + fieldName).hide();
+    $("#update_" + fieldName).hide();
 
-  if (fieldName == "name") {
-    var url = "/change_account/update_name/";
-    var formdata = {
-      first_name: $("#first_name").val(),
-      last_name: $("#last_name").val(),
-    };
-  } else if (fieldName == "email") {
-    var url = "/change_account/update_email/";
-    var formdata = {
-      email: $("#email").val(),
-    };
-  } else {
-    var url = "/change_account/update_password/";
-    var formdata = {
-      password: $("#password").val(),
-    };
-  }
+    if (fieldName == "name") {
+        var url = "/change_account/update_name/";
+        var formdata = {
+            first_name: $("#first_name").val(),
+            last_name: $("#last_name").val(),
+        };
+    } else if (fieldName == "email") {
+        var url = "/change_account/update_email/";
+        var formdata = {
+            email: $("#email").val(),
+        };
+    } else {
+        var url = "/change_account/update_password/";
+        var formdata = {
+            password: $("#password").val(),
+        };
+    }
 
-  $.ajax({
-    type: "POST",
-    headers: { "X-CSRFToken": csrf_token },
-    url: url,
-    data: formdata,
-    success: function (response) {
-      console.log(response);
-      ajaxResponse(fieldName, formdata);
-    },
-    error: function (xhr, errmsg, err) {},
-  });
+    $.ajax({
+        type: "POST",
+        headers: { "X-CSRFToken": csrf_token },
+        url: url,
+        data: formdata,
+        success: function (response) {
+            console.log(response);
+            ajaxResponse(fieldName, formdata);
+        },
+        error: function (xhr, errmsg, err) {},
+    });
 }
 
 function ajaxResponse(fieldName, formdata) {
-  if (fieldName == "name") {
-    $(`#${fieldName}_text`).text(
-      "Name: " + formdata["first_name"] + " " + formdata["last_name"]
-    );
-  } else if (fieldName == "email") {
-    $(`#${fieldName}_text`).text("Email: " + formdata["email"]);
-  } else {
-    $(`#${fieldName}_text`).text("New password set.");
-  }
+    if (fieldName == "name") {
+        $(`#${fieldName}_text`).text(
+            "Name: " + formdata["first_name"] + " " + formdata["last_name"]
+        );
+    } else if (fieldName == "email") {
+        $(`#${fieldName}_text`).text("Email: " + formdata["email"]);
+    } else {
+        $(`#${fieldName}_text`).text("New password set.");
+    }
 }
--- cs_app/static/js/generate_report_script.js
+++ cs_app/static/js/generate_report_script.js
@@ -1,15 +1,15 @@
 function submitParameters() {
     var formdata = {
-        'start_date' : $("#start_date").val(),
-        'end_date' : $("#end_date").val(),
-        'client_name' : $("#client_name").val(),
-        'sorting_option' : $("#sorting_option").val(),
-    }
+        start_date: $("#start_date").val(),
+        end_date: $("#end_date").val(),
+        client_name: $("#client_name").val(),
+        sorting_option: $("#sorting_option").val(),
+    };
 
     var res = "";
     for (var key in formdata) {
         res += `${formdata[key]} ,`;
     }
 
-    $("#report_params").text("Report Parameters: " + res.slice(0, -2))
+    $("#report_params").text("Report Parameters: " + res.slice(0, -2));
 }
