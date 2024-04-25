3f5e70b1593b5b8d35c6b54bfcd60833a4ea910b
khowellmobile <khowellmobile@gmailc.com>
2024-04-25 09:45:01 -0400

    Fixed an issue where the table would not fill container div

--- cs_app/static/js/generate_report_script.js
+++ cs_app/static/js/generate_report_script.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
     let table = $("#example").DataTable({
+        "bAutoWidth": false
     })
+
+    setTableSize();
 });
 
 function submitParameters() {
@@ -17,4 +20,18 @@ function submitParameters() {
     }
 
     $("#report_params").text("Report Parameters: " + res.slice(0, -2));
+}
+
+function setTableSize() {
+    let parentWidth = $(".reportBlock").width()
+    let parentHeight = $(".reportBlock").height()
+
+    let childWidth = parentWidth - (parentWidth / 100)
+    let childHeight = parentHeight - (parentHeight / 10)
+
+    console.log(parentHeight + " " + childHeight);
+    console.log(parentWidth + " " + childWidth);
+
+    $("#example").css("height", childHeight + "px")
+    $("#example").css("width", childWidth + "px")
 }
