d23bb41d65c31bf203e865d477ca13788a4008c6
khowellmobile <khowellmobile@gmailc.com>
2024-05-02 13:06:02 -0400

    Created new model for storing past report parameters

--- cs_app/models.py
+++ cs_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import json
 
-# Create your models here.
+class PastParameter(models.Model):
+    text_field = models.TextField()
+    date_field = models.DateField()
+    parameters_json = models.JSONField()
+
+    def set_parameters(self, parameters):
+        self.parameters_json = json.dumps(parameters)
+
+    def get_parameters(self):
+        return json.loads(self.parameters_json)
+
+    def __str__(self):
+        return f"Text: {self.text_field}, Date: {self.date_field}, Parameters: {self.parameters_json}"
