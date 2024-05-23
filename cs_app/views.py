012352eaf5abef6bf08bf0785f625c4d30371f23
khowellmobile <khowellmobile@gmailc.com>
2024-05-23 09:25:47 -0400

    Adjusted remove_conn_and_config code order

--- cs_app/views.py
+++ cs_app/views.py
@@ -240,12 +240,14 @@ def switch_database_view(request):
 
 
 def remove_conn_and_config(alias):
-    if alias in settings.DATABASES:
-        del settings.DATABASES[alias]
+    connections.close_all()
 
     for conn in connections.all():
         if conn.alias == alias:
             connections.__delitem__(alias)
+
+    if alias in settings.DATABASES:
+        del settings.DATABASES[alias]
 
 
 def generate_unique_alias(base_alias):
