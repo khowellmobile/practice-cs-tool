0ed2199f7a105fea5779f3f0d21fc63ceeb86bd6
khowell <khowellmobile@gmail.com>
2024-06-12 12:57:14 -0400

    Changed names and added spinner

--- cs_app/static/js/change_database_script.js
+++ cs_app/static/js/change_database_script.js
@@ -50,13 +50,13 @@ function dbChangeHandler(success, message) {
     setSpinnerVisibility(false);
 
     if (success) {
-        $("#connection-info__status__log").append(
+        $("#database-change__status").append(
             "<p class='stat__message'>Successful Connection</p>"
         );
         getDBInfoFromAlias(message.db_alias);
     } else {
         if (message.responseJSON) {
-            $("#connection-info__status__log").append(
+            $("#database-change__status").append(
                 `<p class='stat__message'> Error: ${message.responseJSON.error}</p>`
             );
         } else {
