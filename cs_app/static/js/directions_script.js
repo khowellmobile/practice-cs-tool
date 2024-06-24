018f361af39edd4479f2ccb479d7427f72959118
khowellmobile <khowellmobile@gmailc.com>
2024-06-24 15:53:46 -0400

    Updated directions files to include fixed and new directions slides

--- cs_app/static/js/directions_script.js
+++ cs_app/static/js/directions_script.js
@@ -1,30 +1,44 @@
+var currentActiveId = "t1";
+var lockTabs = false;
+
 $(".tab").on("click", function () {
-    let e = $(this);
-    let tabId = e.attr("id");
+    if (!lockTabs) {
+        lockTabs = true;
+        setTimeout(() => {
+            lockTabs = false;
+        }, 2400);
 
-    $(".tab").each(function (index, element) {
-        if ($(element).attr("id") === tabId) {
-            $(element).removeClass("inactive");
-            $(element).addClass("active");
-        } else {
-            $(element).removeClass("active");
-            $(element).addClass("inactive");
-        }
-    });
+        hideActive();
 
-    toggleSlideHandler(e);
+        let e = $(this);
+        let tabId = e.attr("id");
+        currentActiveId = tabId;
+
+        $(".tab").each(function (index, element) {
+            if ($(element).attr("id") === tabId) {
+                $(element).removeClass("inactive");
+                $(element).addClass("active");
+            } else {
+                $(element).removeClass("active");
+                $(element).addClass("inactive");
+            }
+        });
+        showNewActive();
+    } else {
+        console.log("locked");
+    }
 });
 
-function toggleSlideHandler(tab) {
-    switch (tab.attr("id")) {
+function hideActive() {
+    switch (currentActiveId) {
         case "t1":
-            toggleOverviewSlides();
+            hideOverviewSlides();
             break;
         case "t2":
-            toggleOverviewSlides();
+            hideGenRepSlides();
             break;
         case "t3":
-            toggleOverviewSlides();
+            hideChangeDBSlides();
             break;
         case "t4":
             toggleOverviewSlides();
@@ -34,38 +48,130 @@ function toggleSlideHandler(tab) {
     }
 }
 
-function toggleOverviewSlides() {
+function showNewActive() {
+    switch (currentActiveId) {
+        case "t1":
+            setTimeout(() => {
+                showOverviewSlides();
+            }, 1200);
+            break;
+        case "t2":
+            setTimeout(() => {
+                showGenRepSlides();
+            }, 1200);
+            break;
+        case "t3":
+            setTimeout(() => {
+                showChangeDBSlides();
+            }, 1200);
+            break;
+        default:
+            console.log("unknown tab");
+    }
+}
+
+function hideOverviewSlides() {
     let e1 = $("#d1-2-1");
     let e2 = $("#d1-2-2");
     let e3 = $("#d1-2-3");
+    setTimeout(() => {
+        e1.addClass("up");
+    }, 150);
+    setTimeout(() => {
+        e2.addClass("down");
+    }, 300);
+    setTimeout(() => {
+        e3.addClass("up");
+    }, 450);
+    setTimeout(() => {
+        e1.css("display", "none");
+        e2.css("display", "none");
+        e3.css("display", "none");
+    }, 1200);
+}
 
-    if (e1.hasClass("up")) {
-        setTimeout(() => {
-            e1.removeClass("up");
-        }, 150);
-        setTimeout(() => {
-            e2.removeClass("down");
-        }, 300);
-        setTimeout(() => {
-            e3.removeClass("up");
-        }, 450);
-        setTimeout(() => {
-            e1.css("background-color", "lightgreen");
-            e2.css("background-color", "lightgreen");
-            e3.css("background-color", "lightgreen");
-        }, 1200);
-    } else {
-        e1.css("background-color", "red");
-        e2.css("background-color", "red");
-        e3.css("background-color", "red");
-        setTimeout(() => {
-            e1.addClass("up");
-        }, 150);
-        setTimeout(() => {
-            e2.addClass("down");
-        }, 300);
-        setTimeout(() => {
-            e3.addClass("up");
-        }, 450);
-    }
+function showOverviewSlides() {
+    let e1 = $("#d1-2-1");
+    let e2 = $("#d1-2-2");
+    let e3 = $("#d1-2-3");
+    e1.css("display", "flex");
+    e2.css("display", "flex");
+    e3.css("display", "flex");
+
+    setTimeout(() => {
+        e1.removeClass("up");
+    }, 150);
+    setTimeout(() => {
+        e2.removeClass("down");
+    }, 300);
+    setTimeout(() => {
+        e3.removeClass("up");
+    }, 450);
+}
+
+function hideGenRepSlides() {
+    let e1 = $("#d1-2-4");
+    let e2 = $("#d1-2-5");
+    setTimeout(() => {
+        e1.addClass("up");
+    }, 150);
+    setTimeout(() => {
+        e2.addClass("down");
+    }, 300);
+    setTimeout(() => {
+        e1.css("display", "none");
+        e2.css("display", "none");
+    }, 1200);
+}
+
+function showGenRepSlides() {
+    let e1 = $("#d1-2-4");
+    let e2 = $("#d1-2-5");
+    e1.css("display", "flex");
+    e2.css("display", "flex");
+    setTimeout(() => {
+        e1.removeClass("up");
+    }, 150);
+    setTimeout(() => {
+        e2.removeClass("down");
+    }, 300);
+}
+
+function hideChangeDBSlides() {
+    let e1 = $("#d1-2-6");
+    let e2 = $("#d1-2-7");
+    let e3 = $("#d1-2-8");
+    setTimeout(() => {
+        e1.addClass("down");
+    }, 150);
+    setTimeout(() => {
+        e2.addClass("up");
+    }, 300);
+    setTimeout(() => {
+        e3.addClass("down");
+    }, 450);
+    setTimeout(() => {
+        e1.css("display", "none");
+        e2.css("display", "none");
+        e3.css("display", "none");
+    }, 1200);
+}
+
+function showChangeDBSlides() {
+    let e1 = $("#d1-2-6");
+    let e2 = $("#d1-2-7");
+    let e3 = $("#d1-2-8");
+    e1.css("display", "flex");
+    e2.css("display", "flex");
+    e3.css("display", "flex");
+
+    setTimeout(() => {
+        e1.removeClass("down");
+    }, 150);
+    setTimeout(() => {
+        e2.removeClass("up");
+    }, 300);
+    setTimeout(() => {
+        e3.removeClass("down");
+    }, 450);
 }
