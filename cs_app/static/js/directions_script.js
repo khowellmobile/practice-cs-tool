5a83a5ab8d601a61faf4d2898f10d5df3d897bb2
khowellmobile <khowellmobile@gmailc.com>
2024-06-24 15:16:21 -0400

    tinkered slide animations

--- cs_app/static/js/directions_script.js
+++ cs_app/static/js/directions_script.js
@@ -4,14 +4,35 @@ $(".tab").on("click", function () {
 
     $(".tab").each(function (index, element) {
         if ($(element).attr("id") === tabId) {
-            $(element).removeClass("inactive"); 
+            $(element).removeClass("inactive");
             $(element).addClass("active");
         } else {
             $(element).removeClass("active");
             $(element).addClass("inactive");
         }
     });
+
+    toggleSlideHandler(e);
 });
+
+function toggleSlideHandler(tab) {
+    switch (tab.attr("id")) {
+        case "t1":
+            toggleOverviewSlides();
+            break;
+        case "t2":
+            toggleOverviewSlides();
+            break;
+        case "t3":
+            toggleOverviewSlides();
+            break;
+        case "t4":
+            toggleOverviewSlides();
+            break;
+        default:
+            console.log("unknown tab");
+    }
+}
 
 function toggleOverviewSlides() {
     let e1 = $("#d1-2-1");
@@ -21,22 +42,30 @@ function toggleOverviewSlides() {
     if (e1.hasClass("up")) {
         setTimeout(() => {
             e1.removeClass("up");
-        }, 300); 
+        }, 150);
         setTimeout(() => {
             e2.removeClass("down");
-        }, 600); 
+        }, 300);
         setTimeout(() => {
             e3.removeClass("up");
-        }, 900); 
+        }, 450);
+        setTimeout(() => {
+            e1.css("background-color", "lightgreen");
+            e2.css("background-color", "lightgreen");
+            e3.css("background-color", "lightgreen");
+        }, 1200);
     } else {
+        e1.css("background-color", "red");
+        e2.css("background-color", "red");
+        e3.css("background-color", "red");
         setTimeout(() => {
             e1.addClass("up");
-        }, 300); 
+        }, 150);
         setTimeout(() => {
             e2.addClass("down");
-        }, 600); 
+        }, 300);
         setTimeout(() => {
             e3.addClass("up");
-        }, 900);
+        }, 450);
     }
 }
--- cs_app/static/js/tinker_script.js
+++ cs_app/static/js/tinker_script.js
@@ -1,27 +1,133 @@
-function foo() {
+var currentActiveId = "t1";
+var lockTabs = false;
+
+$(".tab").on("click", function () {
+    if (!lockTabs) {
+        lockTabs = true;
+        setTimeout(() => {
+            lockTabs = false;
+        }, 2400);
+
+        hideActive();
+
+        let e = $(this);
+        let tabId = e.attr("id");
+        currentActiveId = tabId;
+
+        $(".tab").each(function (index, element) {
+            if ($(element).attr("id") === tabId) {
+                $(element).removeClass("inactive");
+                $(element).addClass("active");
+            } else {
+                $(element).removeClass("active");
+                $(element).addClass("inactive");
+            }
+        });
+        showNewActive();
+    } else {
+        console.log("locked");
+    }
+});
+
+function hideActive() {
+    switch (currentActiveId) {
+        case "t1":
+            hideOverviewSlides();
+            break;
+        case "t2":
+            hideGenRepSlides();
+            break;
+        case "t3":
+            toggleOverviewSlides();
+            break;
+        case "t4":
+            toggleOverviewSlides();
+            break;
+        default:
+            console.log("unknown tab");
+    }
+}
+
+function showNewActive() {
+    switch (currentActiveId) {
+        case "t1":
+            setTimeout(() => {
+                showOverviewSlides();
+            }, 1200);
+            break;
+        case "t2":
+            setTimeout(() => {
+                showGenRepSlides();
+            }, 1200);
+            break;
+        default:
+            console.log("unknown tab");
+    }
+}
+
+function hideOverviewSlides() {
     let e1 = $("#d1-2-1");
     let e2 = $("#d1-2-2");
     let e3 = $("#d1-2-3");
+    setTimeout(() => {
+        e1.addClass("up");
+    }, 150);
+    setTimeout(() => {
+        e2.addClass("down");
+    }, 300);
+    setTimeout(() => {
+        e3.addClass("up");
+    }, 450);
+    setTimeout(() => {
+        e1.css("display", "none");
+        e2.css("display", "none");
+        e3.css("display", "none");
+    }, 1200);
+}
 
-    if (e1.hasClass("up")) {
-        setTimeout(() => {
-            e1.removeClass("up");
-        }, 300); 
-        setTimeout(() => {
-            e2.removeClass("down");
-        }, 600); 
-        setTimeout(() => {
-            e3.removeClass("up");
-        }, 900); 
-    } else {
-        setTimeout(() => {
-            e1.addClass("up");
-        }, 300); 
-        setTimeout(() => {
-            e2.addClass("down");
-        }, 600); 
-        setTimeout(() => {
-            e3.addClass("up");
-        }, 900);
-    }
+function showOverviewSlides() {
+    let e1 = $("#d1-2-1");
+    let e2 = $("#d1-2-2");
+    let e3 = $("#d1-2-3");
+    e1.css("display", "flex");
+    e2.css("display", "flex");
+    e3.css("display", "flex");
+
+    setTimeout(() => {
+        e1.removeClass("up");
+    }, 150);
+    setTimeout(() => {
+        e2.removeClass("down");
+    }, 300);
+    setTimeout(() => {
+        e3.removeClass("up");
+    }, 450);
+}
+
+function hideGenRepSlides() {
+    let e1 = $("#d1-2-4");
+    let e2 = $("#d1-2-5");
+    setTimeout(() => {
+        e1.addClass("up");
+    }, 150);
+    setTimeout(() => {
+        e2.addClass("down");
+    }, 300);
+    setTimeout(() => {
+        e1.css("display", "none");
+        e2.css("display", "none");
+    }, 1200);
+}
+
+function showGenRepSlides() {
+    let e1 = $("#d1-2-4");
+    let e2 = $("#d1-2-5");
+    e1.css("display", "flex");
+    e2.css("display", "flex");
+    setTimeout(() => {
+        e1.removeClass("up");
+    }, 150);
+    setTimeout(() => {
+        e2.removeClass("down");
+    }, 300);
 }
