889d62c3a5dfe87992cb2a7f69d1701676353033
khowellmobile <khowellmobile@gmailc.com>
2024-07-08 13:08:17 -0400

    Adjusted comments and formatting.

--- cs_app/static/js/account_information_script.js
+++ cs_app/static/js/account_information_script.js
@@ -1,5 +1,5 @@
 /**
- * JavaScript file for managing user account information updates via AJAX.
+ * JavaScript file for managing user account information updates via AJAX for the account_information.html page.
  *
  * This file contains functions to toggle display of field change forms, submit form data
  * via AJAX requests to update user information (name, email, password), and handle successful
@@ -15,7 +15,6 @@
  * Dependencies: Requires jQuery for DOM manipulation and AJAX requests.
  */
 
-
 /**
  * Toggles the visibility of the form used to update the specified field.
  *
@@ -25,10 +24,9 @@ function toggleForm(fieldName) {
     $("#update_" + fieldName).toggle();
 }
 
-
 /**
  * Submits form data for updating user information via AJAX based on the field name (name, email, password).
- * 
+ *
  * The passed fieldName will determine the format of that data and the view the data is sent to.
  *
  * @param {string} fieldName - The name of the field to update (name, email, password).
@@ -80,7 +78,8 @@ function ajaxResponseSuccess(fieldName, formdata) {
     var displayText;
 
     if (fieldName == "name") {
-        displayText = "Name: " + formdata["first_name"] + " " + formdata["last_name"];
+        displayText =
+            "Name: " + formdata["first_name"] + " " + formdata["last_name"];
     } else if (fieldName == "email") {
         displayText = "Email: " + formdata["email"];
     } else {
