5b4f12b0e3a4a7ec5fa8b6f23afb0272b8705c2d
khowellmobile <khowellmobile@gmailc.com>
2024-07-09 08:49:16 -0400

    Added comments to account_information_script.js

--- cs_app/static/js/account_information_script.js
+++ cs_app/static/js/account_information_script.js
@@ -1,9 +1,36 @@
-// Toggles display of field change form
+/**
+ * JavaScript file for managing user account information updates via AJAX for the account_information.html page.
+ *
+ * This file contains functions to toggle display of field change forms, submit form data
+ * via AJAX requests to update user information (name, email, password), and handle successful
+ * AJAX responses by updating corresponding elements on the page dynamically.
+ *
+ * Functions:
+ * - toggleForm(fieldName): Toggles the display of the form for updating the specified field.
+ * - submitForm(fieldName): Submits form data for updating user information via AJAX based on
+ *                           the field name (name, email, password).
+ * - ajaxResponseSuccess(fieldName, formdata): Updates UI elements with new information upon
+ *                                             successful AJAX response for the specified field.
+ *
+ * Dependencies: Requires jQuery for DOM manipulation and AJAX requests.
+ */
+
+/**
+ * Toggles the visibility of the form used to update the specified field.
+ *
+ * @param {string} fieldName - The name of the field for which the form should be toggled.
+ */
 function toggleForm(fieldName) {
     $("#update_" + fieldName).toggle();
 }
 
-// Detects user change and submits to view for saving
+/**
+ * Submits form data for updating user information via AJAX based on the field name (name, email, password).
+ *
+ * The passed fieldName will determine the format of that data and the view the data is sent to.
+ *
+ * @param {string} fieldName - The name of the field to update (name, email, password).
+ */
 function submitForm(fieldName) {
     $("#update_" + fieldName).hide();
 
@@ -37,7 +64,12 @@ function submitForm(fieldName) {
     });
 }
 
-// Shows new fieldInformation. Called on successful ajax call
+/**
+ * Updates UI elements with new information upon successful AJAX response for the specified field.
+ *
+ * @param {string} fieldName - The name of the field that was updated (name, email, password).
+ * @param {Object} formdata - The data object containing updated field information.
+ */
 function ajaxResponseSuccess(fieldName, formdata) {
     if (fieldName == "name") {
         $(`#${fieldName}_text`).text(
