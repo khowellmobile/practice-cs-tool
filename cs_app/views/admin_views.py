62b5fd34cbf4496fb1a3ff6e6775433badd6423e
khowellmobile <khowellmobile@gmailc.com>
2024-07-12 11:59:47 -0400

    Added comments to admin_views.py

--- cs_app/views/admin_views.py
+++ cs_app/views/admin_views.py
@@ -1,3 +1,21 @@
+"""
+Django views for main, home, and tinker pages and related functionalities.
+
+This module contains Django view functions for rendering main page, home page, and
+tinker page. The views for home and tinker pages require user authentication using
+the @login_required decorator to ensure access only to authenticated users.
+
+Functions:
+- main_view(request): Renders the main page using 'index.html' template.
+- home_view(request): Renders the home page with user information if authenticated.
+- tinker_view(request): Renders the tinker page with user information if authenticated.
+
+Dependencies:
+- Django modules: HttpResponse, loader
+- Django shortcuts: render
+- Django authentication: login_required
+"""
+
 from django.http import HttpResponse
 from django.template import loader
 
@@ -6,12 +24,34 @@ from django.shortcuts import render
 
 
 def main_view(request):
+    """
+    View function to render the main page.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: Renders the 'index.html' template.
+    """
+    
     template = loader.get_template("index.html")
     return HttpResponse(template.render())
 
 
 @login_required
 def home_view(request):
+    """
+    View function to render the home page for authenticated users.
+
+    Requires the user to be logged in to access the view.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: Renders the 'home.html' template with user context.
+    """
+
     user = request.user
     context = {
         "user": user,
@@ -22,6 +62,16 @@ def home_view(request):
 
 @login_required
 def tinker_view(request):
+    """
+    View function to render the tinker page for authenticated users.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: Renders the 'tinker.html' template with user context.
+    """
+
     user = request.user
     context = {
         "user": user,
