c80f6152d652b10887fdba7772117f915db5bc24
khowellmobile <khowellmobile@gmailc.com>
2024-07-12 12:35:32 -0400

    Added comments to init.py in views folder

--- cs_app/views/__init__.py
+++ cs_app/views/__init__.py
@@ -1,6 +1,17 @@
+# Import all admin-related views
 from .admin_views import *
+
+# Import all authorization-related views
 from .authorization_views import *
+
+# Import all generate report-related views
 from .generate_report_views import *
+
+# Import all directions-related views
 from .directions_views import *
+
+# Import all change database-related views
 from .change_database_views import *
+
+# Import all account information-related views
 from .account_information_views import *
