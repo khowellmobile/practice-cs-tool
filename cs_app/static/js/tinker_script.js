d281ebb2d6a5ba3073c4c24540cc0bf83c0a556f
khowellmobile <khowellmobile@gmailc.com>
2024-07-29 09:17:28 -0400

    Changed slider step size

--- cs_app/static/js/tinker_script.js
+++ cs_app/static/js/tinker_script.js
@@ -10,12 +10,13 @@ $(document).ready(function () {
                 <div id="s${i}" class="input-container">
                     <input
                         type="range"
-                        min="0"
-                        max="100"
-                        value="50"
+                        min="0.00"
+                        max="5.00"
+                        step="0.01"
+                        value="1.00"
                         class="sliderRange "
                     />
-                    <input type="text" value="50" class="sliderInput"/>
+                    <input type="text" value="1.0" class="sliderInput"/>
                 </div>
             </div>
         `;
@@ -27,7 +28,7 @@ $(document).ready(function () {
         let parentId = $(this).parent().attr("id");
         $("sliderRange").val(sliderInputRange($(this).val(), parentId));
     });
-    
+
     $(".sliderRange").on("input", function () {
         let parentId = $(this).parent().attr("id");
         e = $("#" + parentId + " > input[type=text]");
@@ -36,12 +37,16 @@ $(document).ready(function () {
 });
 
 function sliderInputRange(num, id) {
-    e = $("#" + id + " > input[type=range]");
-    if (num > 100) {
-        e.val(100);
+    e1 = $("#" + id + " > input[type=range]");
+    e2 = $("#" + id + " > input[type=text]");
+    if (num > 5) {
+        e1.val(5);
+        e2.val(5);
     } else if (num < 0) {
-        e.val(0);
+        e1.val(0);
+        e2.val(0);
     } else {
-        e.val(num);
+        e1.val(num);
+        e2.val(num);
     }
 }
