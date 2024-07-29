cdecb64e06306e54059fe0e60636ff3b23994ddf
khowellmobile <khowellmobile@gmailc.com>
2024-07-29 10:07:35 -0400

    Added buttons

--- cs_app/static/js/tinker_script.js
+++ cs_app/static/js/tinker_script.js
@@ -1,28 +1,7 @@
 $(document).ready(function () {
-    // Number of sliders to add
-    var numberOfSliders = 9;
-
-    // Loop to create and append the sliders
-    for (var i = 1; i <= numberOfSliders; i++) {
-        var sliderHtml = `
-            <div class="slider-container">
-                <p>s${i}</p>
-                <div id="s${i}" class="input-container">
-                    <input
-                        type="range"
-                        min="0.00"
-                        max="5.00"
-                        step="0.01"
-                        value="1.00"
-                        class="sliderRange "
-                    />
-                    <input type="text" value="1.0" class="sliderInput"/>
-                </div>
-            </div>
-        `;
-
-        $("#d1-1").append(sliderHtml);
-    }
+    
+    populateSliders();
+    populateButtons();
 
     $(".sliderInput").on("change", function () {
         let parentId = $(this).parent().attr("id");
@@ -48,5 +27,43 @@ function sliderInputRange(num, id) {
     } else {
         e1.val(num);
         e2.val(num);
+    }
+}
+
+function populateSliders() {
+    // Number of sliders to add
+    var numberOfSliders = 9;
+
+    // Loop to create and append the sliders
+    for (var i = 1; i <= numberOfSliders; i++) {
+        var sliderHtml = `
+            <div class="slider-container">
+                <p>s${i}</p>
+                <div id="s${i}" class="input-container">
+                    <input
+                        type="range"
+                        min="0.00"
+                        max="5.00"
+                        step="0.01"
+                        value="1.00"
+                        class="sliderRange "
+                    />
+                    <input type="text" value="1.0" class="sliderInput"/>
+                </div>
+            </div>
+        `;
+
+        $("#d1-1").append(sliderHtml);
+    }
+}
+
+function populateButtons() {
+    // Loop to create and append the buttons
+    for (var i = 1; i <= 10; i++) {
+        var buttonHtml = `
+            <button id="b-${i}" class="button-1">Place Holder</button>
+        `;
+
+        $("#d2-1").append(buttonHtml);
     }
 }
