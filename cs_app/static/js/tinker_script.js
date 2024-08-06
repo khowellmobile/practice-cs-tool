3ca861df2a60caa1f3740e2fd205e953215323d4
khowellmobile <khowellmobile@gmailc.com>
2024-08-06 10:15:35 -0400

    changed sliders to make range customizable

--- cs_app/static/js/tinker_script.js
+++ cs_app/static/js/tinker_script.js
@@ -26,19 +26,24 @@ var sliderStates = {
 var activeElement = "";
 
 $(document).ready(function () {
-    //populateSliders();
+    populateSliders();
     populateButtons();
     populateSliderAssigns();
 
-    $(".sliderInput").on("change", function () {
-        let parentId = $(this).parent().attr("id");
-        $("sliderRange").val(sliderInputRange($(this).val(), parentId));
-    });
-
     $(".sliderRange").on("input", function () {
         let parentId = $(this).parent().attr("id");
-        e = $("#" + parentId + " > input[type=text]");
+        e = $("#" + parentId).prev("input");
         e.val($(this).val());
+    });
+
+    $(".slider-start").on("change", function () {
+        e = $(this).next("input");
+        e.attr("min", $(this).val());
+    });
+
+    $(".slider-end").on("change", function () {
+        e = $(this).prev("input");
+        e.attr("max", $(this).val());
     });
 
     iterateChildren("#dev-container", "", "k", "1");
@@ -84,21 +89,6 @@ $(document).ready(function () {
     });
 });
 
-function sliderInputRange(num, id) {
-    e1 = $("#" + id + " > input[type=range]");
-    e2 = $("#" + id + " > input[type=text]");
-    if (num > 5) {
-        e1.val(5);
-        e2.val(5);
-    } else if (num < 0) {
-        e1.val(0);
-        e2.val(0);
-    } else {
-        e1.val(num);
-        e2.val(num);
-    }
-}
-
 function populateSliders() {
     // Number of sliders to add
     var numberOfSliders = 9;
@@ -107,17 +97,19 @@ function populateSliders() {
     for (var i = 1; i <= numberOfSliders; i++) {
         var sliderHtml = `
             <div class="slider-container">
-                <p>s-${i}</p>
+                <span>s-${i}</span>
+                <input type="text" value="1.0" class="sliderInput"/>
                 <div id="s-${i}" class="input-container">
+                    <input type="text" value="0.0" class="slider-start"/>
                     <input
                         type="range"
                         min="0.00"
-                        max="5.00"
+                        max="10.0"
                         step="0.01"
                         value="1.00"
                         class="sliderRange"
                     />
-                    <input type="text" value="1.0" class="sliderInput"/>
+                    <input type="text" value="10.0" class="slider-end"/>
                 </div>
             </div>
         `;
