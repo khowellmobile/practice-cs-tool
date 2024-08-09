a0841e1162eb3c84117d6fb227065188ff81e3a4
khowellmobile <khowellmobile@gmailc.com>
2024-08-09 10:03:56 -0400

    Rearreanged function ordering

--- cs_app/static/js/tinker_script.js
+++ cs_app/static/js/tinker_script.js
@@ -11,87 +11,35 @@ var buttonAssignments = {
     "b-10": null,
 };
 
+// Generic object added for clarity. 
+// Other objects added in populateLeafs().
 var leafStates = {
-    "k-l": {
+    "k-?-l": {
         "k-value": null,
         "slider-id": null,
     },
 };
 
+// Generic object added for clarity. 
+// Other objects added in populateSliders().
 var sliderStates = {
-    "s-1": {
-        "slider-assign-id": "s-1-a",
+    "s-?": {
+        "slider-assign-id": "s-?-a",
         "k-l": null,
         units: null,
         property: null,
         assigned: false,
-    },
-    "s-2": {
-        "slider-assign-id": "s-2-a",
-        "k-l": null,
-        units: null,
-        property: null,
-        assigned: false,
-    },
-    "s-3": {
-        "slider-assign-id": "s-3-a",
-        "k-l": null,
-        units: null,
-        property: null,
-        assigned: false,
-    },
-    "s-4": {
-        "slider-assign-id": "s-4-a",
-        "k-l": null,
-        units: null,
-        property: null,
-        assigned: false,
-    },
-    "s-5": {
-        "slider-assign-id": "s-5-a",
-        "k-l": null,
-        units: null,
-        property: null,
-        assigned: false,
-    },
-    "s-6": {
-        "slider-assign-id": "s-6-a",
-        "k-l": null,
-        units: null,
-        property: null,
-        assigned: false,
-    },
-    "s-7": {
-        "slider-assign-id": "s-7-a",
-        "k-l": null,
-        units: null,
-        property: null,
-        assigned: false,
-    },
-    "s-8": {
-        "slider-assign-id": "s-8-a",
-        "k-l": null,
-        units: null,
-        property: null,
-        assigned: false,
-    },
-    "s-9": {
-        "slider-assign-id": "s-9-a",
-        "k-l": null,
-        units: null,
-        property: null,
-        assigned: false,
-    },
+    }
 };
 
 var activeElement = "";
 
 $(document).ready(function () {
     // Populating page with needed elements
-    populateSliders();
-    populateButtons();
-    populateSliderAssigns();
     populateLeafs("#dev-container", "", "k", "1");
+    populateSliders();
+    populateSliderAssigns();
+    populateButtons();
 
     // Attaching listeners to sliders
     attachSliderActionHandlers();
@@ -109,7 +57,42 @@ $(document).ready(function () {
 });
 
 function foo() {
-    console.log(leafStates);
+    console.log(sliderStates);
+}
+
+function populateLeafs(identifier, indent, k, i) {
+    $element = $(identifier);
+    eId = $element.attr("id");
+    eTag = $element.prop("tagName");
+
+    $element.attr("k-value", k);
+
+    var leaf = getLeaf(eId, eTag, indent, k);
+
+    leafStates[k + "-l"] = {
+        "k-value": k,
+        "slider-id": null,
+    };
+
+    $("#tree-container").append(leaf);
+
+    $element.children().each(function () {
+        // Recursively call the function for each child
+        populateLeafs($(this), indent + "---", k + "-" + i++, 1);
+    });
+}
+
+function getLeaf(eId, eTag, indent, k) {
+    if (eId == undefined) {
+        eId = "";
+    }
+
+    var leaf = `<div class="leaf" k-value='${k}-l'>
+            <div>${indent}<p>${eTag} ${eId}</p></div><div class='indicators-div'></div>
+        </div>
+    `;
+
+    return leaf;
 }
 
 function populateSliders() {
@@ -138,6 +121,14 @@ function populateSliders() {
         `;
 
         $("#d1-1").append(sliderHtml);
+
+        sliderStates["s-" + i] = {
+            "slider-assign-id": `s-${i}-a`,
+            "k-l": null,
+            units: null,
+            property: null,
+            assigned: false,
+        };
     }
 }
 
@@ -161,6 +152,44 @@ function populateSliderAssigns() {
             `
         );
     }
+}
+
+function attachLeafActionHandlers() {
+    $(".leaf").on("mouseenter", function () {
+        let k = $(this).attr("k-value").slice(0, -2);
+
+        $(`[k-value='${k}']`).addClass("hovered");
+    });
+
+    $(".leaf").on("click", function () {
+        // Getting/setting variables
+        const k = $(this).attr("k-value").slice(0, -2);
+        const element = $(`[k-value='${k}']`);
+        const tagName = element.prop("tagName");
+        const id = element.attr("id") || "No ID";
+        const classList = element.attr("class").split(/\s+/) || "No Classes";
+
+        activeElement = element;
+
+        $(this).toggleClass("clicked");
+
+        $("#outline-check").prop("checked", element.hasClass("outlineP"));
+
+        // Displaying needed information
+        $("#e-classes").empty();
+
+        for (e in classList) {
+            $("#e-classes").append(`<li>${classList[e]}</li>`);
+        }
+        $("#e-tag").html(`<b>Tag Name: </b>${tagName}`);
+        $("#e-id").html(`<b>Id: </b>${id}`);
+    });
+
+    $(".leaf").on("mouseleave", function () {
+        let k = $(this).attr("k-value").slice(0, -2);
+
+        $(`[k-value='${k}']`).removeClass("hovered");
+    });
 }
 
 function populateButtons() {
@@ -220,7 +249,6 @@ function attachSliderActionHandlers() {
         sliderStates[sliderId]["units"] = $(this).val();
     });
 
-    // TODO -----------------------------------//
     $(".grid-item .dot").on("click", function () {
         sliderId = $(this).parent().attr("id").slice(0, -2);
         sliderStates[sliderId]["k-l"] = activeElement.attr("k-l");
@@ -232,101 +260,6 @@ function attachSliderActionHandlers() {
         }
 
         $(this).toggleClass("dot-hallow dot-solid");
-    });
-}
-
-function attachLeafActionHandlers() {
-    $(".leaf").on("mouseenter", function () {
-        let k = $(this).attr("k-value").slice(0, -2);
-
-        $(`[k-value='${k}']`).addClass("hovered");
-    });
-
-    $(".leaf").on("click", function () {
-        // Getting/setting variables
-        const k = $(this).attr("k-value").slice(0, -2);
-        const element = $(`[k-value='${k}']`);
-        const tagName = element.prop("tagName");
-        const id = element.attr("id") || "No ID";
-        const classList = element.attr("class").split(/\s+/) || "No Classes";
-
-        activeElement = element;
-
-        $(this).toggleClass("clicked");
-
-        $("#outline-check").prop("checked", element.hasClass("outlineP"));
-
-        // Displaying needed information
-        $("#e-classes").empty();
-
-        for (e in classList) {
-            $("#e-classes").append(`<li>${classList[e]}</li>`);
-        }
-        $("#e-tag").html(`<b>Tag Name: </b>${tagName}`);
-        $("#e-id").html(`<b>Id: </b>${id}`);
-    });
-
-    $(".leaf").on("mouseleave", function () {
-        let k = $(this).attr("k-value").slice(0, -2);
-
-        $(`[k-value='${k}']`).removeClass("hovered");
-    });
-}
-
-function populateLeafs(identifier, indent, k, i) {
-    $element = $(identifier);
-    eId = $element.attr("id");
-    eTag = $element.prop("tagName");
-
-    $element.attr("k-value", k);
-
-    var leaf = getLeaf(eId, eTag, indent, k);
-
-    leafStates[k + "-l"] = {
-        "k-value": k,
-        "slider-id": null,
-    };
-
-    $("#tree-container").append(leaf);
-
-    $element.children().each(function () {
-        // Recursively call the function for each child
-        populateLeafs($(this), indent + "---", k + "-" + i++, 1);
-    });
-}
-
-function getLeaf(eId, eTag, indent, k) {
-    if (eId == undefined) {
-        eId = "";
-    }
-
-    var leaf = `<div class="leaf" k-value='${k}-l'>
-            <div>${indent}<p>${eTag} ${eId}</p></div><div class='indicators-div'></div>
-        </div>
-    `;
-
-    return leaf;
-}
-
-function toggleOutline(outlineOn) {
-    kL = activeElement.attr("k-value") + "-l";
-
-    if (outlineOn) {
-        activeElement.addClass("outlineP");
-        $(`[k-value='${kL}'] > .indicators-div`).append("<div class='blue-box'></div>");
-    } else {
-        activeElement.removeClass("outlineP");
-        $(`[k-value='${kL}'] > .indicators-div .blue-box`).remove();
-    }
-}
-
-function toggleOutlineAll(identifier) {
-    $element = $(identifier);
-
-    $element.toggleClass("outlineR");
-
-    $element.children().each(function () {
-        toggleOutlineAll($(this));
     });
 }
 
@@ -352,6 +285,28 @@ function deactivateSlider(sliderId) {
     sliderStates[sliderId]["assigned"] = false;
 
     $(`#dot-${sliderNum}`).remove();
+}
+
+function toggleOutline(outlineOn) {
+    kL = activeElement.attr("k-value") + "-l";
+
+    if (outlineOn) {
+        activeElement.addClass("outlineP");
+        $(`[k-value='${kL}'] > .indicators-div`).append("<div class='blue-box'></div>");
+    } else {
+        activeElement.removeClass("outlineP");
+        $(`[k-value='${kL}'] > .indicators-div .blue-box`).remove();
+    }
+}
+
+function toggleOutlineAll(identifier) {
+    $element = $(identifier);
+
+    $element.toggleClass("outlineR");
+
+    $element.children().each(function () {
+        toggleOutlineAll($(this));
+    });
 }
 
 function changeCss(parentId, val) {
