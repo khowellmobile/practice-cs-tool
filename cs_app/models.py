51e29207595a22b9b2dc8d64cf992de171a53652
khowellmobile <khowellmobile@gmailc.com>
2024-10-17 08:44:52 -0400

    made and applying migrations

--- cs_app/models.py
+++ cs_app/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 import json
 
@@ -5,3 +6,18 @@ class PastParameter(models.Model):
     text_field = models.TextField()
     date_field = models.DateField()
     parameters_json = models.JSONField()
+
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    company = models.CharField(max_length=100, blank=True, null=True)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name='customuser_groups', 
+        blank=True,
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='customuser_permissions',  
+        blank=True,
+    )
