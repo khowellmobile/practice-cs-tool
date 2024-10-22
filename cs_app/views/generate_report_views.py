cc1d69076fde03a59db35d56de963ad5649bb495
khowellmobile <khowellmobile@gmailc.com>
2024-10-22 16:14:30 -0400

    Initial setup and layout

--- cs_app/views/generate_report_views.py
+++ cs_app/views/generate_report_views.py
@@ -59,6 +59,33 @@ def generate_report_view(request):
 
     return render(request, "generate_report.html", context)
 
+@login_required
+def generate_report_sub_view(request):
+    """
+    View function to generate the generate report page
+
+    Requires the user to be logged in to access the view.
+
+    Retrieves the latest 25 entries from PastParameter ordered by date_field,
+    prepares the data along with the current user's information, and renders
+    the 'generate_report.html' template with the context.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: Rendered template with user and data context.
+    """
+    data = list(PastParameter.objects.order_by("-date_field")[:25])[::-1]
+    user = request.user
+    context = {
+        "user": user,
+        "data": data,
+        "additionalInfo": request.GET.get("additionalInfo", None),
+    }
+
+    return render(request, "subpages/generate_report_sub.html", context)
+
 
 @login_required
 def load_table_view(request):
