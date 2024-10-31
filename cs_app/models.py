0af6ae7cbb0c3b2d2ca6fcae4b524fb540255163
khowellmobile <khowellmobile@gmailc.com>
2024-10-31 09:15:00 -0400

    Added new model to store users previous connected database information

--- cs_app/models.py
+++ cs_app/models.py
@@ -2,11 +2,18 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 import json
 
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    company = models.CharField(max_length=100, blank=True, null=True)
+
 class PastParameter(models.Model):
     text_field = models.TextField()
     date_field = models.DateField()
     parameters_json = models.JSONField()
 
-class User(AbstractUser):
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
-    company = models.CharField(max_length=100, blank=True, null=True)
+class DatabastConnections(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="database_connections")
+    engine = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    host = models.CharField(max_length=100)
+    driver = models.CharField(max_length=100)
