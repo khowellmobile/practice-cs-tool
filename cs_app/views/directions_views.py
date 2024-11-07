f12c0c2edc68d1eb34193ae92f6b864e6d238b17
khowellmobile <khowellmobile@gmailc.com>
2024-11-07 11:30:20 -0500

    removed login necessity

--- cs_app/views/directions_views.py
+++ cs_app/views/directions_views.py
@@ -17,7 +17,6 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
-@login_required
 def directions_view(request):
     """
     View function to render the directions page.
